efc577f34ffda7325b2fbb111fcbe758cb66aef2
unknown <heritoky522@gmai.com>
2024-07-21 16:45:43 +0200

    second commit

--- app/portfolio/project1/components/Components.jsx
+++ app/portfolio/project1/components/Components.jsx
@@ -1,18 +1,36 @@
 "use client"
 
+import Image from "next/image"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
 
-export function VoluteerCard(){
-    return(
-        <div>
-            Volunteer card
+export function Card({linkBtn,bgBtn }) {
+    return (
+        <div className={`flex items-center bg-slate-300 rounded-lg shadow-lg p-4`} >
+            {/* left */}
+            <div className="mobile">
+                <h3 className="h3 text-slate-900 dark:text-white capitalize">Boot Camp Program</h3>
+                <h2 className="">2022-2023 : Volunteer Leader </h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis doloribus repudiandae facilis neque, aspernatur inventore tenetur omnis saepe eos quisquam voluptates dolores beatae eius a doloremque esse officia nobis. Ad?</p>
+                <Link href={linkBtn}>
+                    <Button className={bgBtn} >Read More</Button>
+                </Link>
+            </div>
+            {/* right bg-image front-image*/}
+            <div>
+                <div>
+
+                </div>
+            </div>
+
         </div>
     )
 }
 
-export function Toky(){
-    return(
+export function Toky() {
+    return (
         <div>
-            Toky 
+            Toky
         </div>
     )
 }
